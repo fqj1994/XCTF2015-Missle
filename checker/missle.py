597effd6b63656fe359bc458a639573b0f8820b3
Qijiang Fan <fqj1994@gmail.com>
2015-07-09 17:35:45 +0800

    checker update

--- checker/missle.py
+++ checker/missle.py
@@ -4,11 +4,15 @@ import os
 import json
 import string
 import requests
+import subprocess
 import hashlib
 
 passwords = {
         "self": "123456"
         }
+
+sshs = [] # 'IPs'
+reverse_ports = [9000:9100]
 
 
 def checker(*kwargs):
@@ -23,18 +27,23 @@ def real_checker(host, port, flag, team):
         return {'status': 'down', 'msg': 'wrong launch UI'}
     user = 'admin'
     pwd = passwords[team]
+    reverse_host = random.choise(sshs)
+    reverse_port = random.choice(reverse_ports)
     target = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
     ws = websocket.create_connection("ws://" + host + ":" + str(port) + "/missle")
+    reverse_proxy =subprocess.Popen("ssh -R 9999:localhost:" + str(reverse_port) + " root@" + reverse_host)
     sshd = os.popen("./missle_sshd.erl " + flag, "r", 1)
     assert sshd.readline().strip() == 'ready'
     ws.send(json.dumps(
-        [user, pwd, "127.0.0.1", 9999, target]))
+        [user, pwd, reverse_host, reverse_port, target]))
     first_status = ws.recv_data_frame()[1].data
     if first_status != "Request Received":
         sshd.close()
+        reverse_proxy.kill()
         return {'status': 'down', 'msg': 'Checker cannot authenticate with the service.'}
     sshd_st = sshd.readline().strip()
     if sshd_st != 'ok':
+        reverse_proxy.kill()
         return {'status': 'down', 'msg': sshd_st}
     else:
         rnd_suffix = sshd.readline().strip()
@@ -43,16 +52,23 @@ def real_checker(host, port, flag, team):
         launch_msg = 'launch-missle '  + target
         success_msg = 'Successfully destroyed target ' + target + '-' + rnd_suffix
         if ws.recv_data_frame()[1].data.strip() != welcome_msg:
+            reverse_proxy.kill()
             return {'status': 'down', 'msg': 'Wrong missle'}
         if ws.recv_data_frame()[1].data.strip() != launch_msg: 
+            reverse_proxy.kill()
             return {'status': 'down', 'msg': 'What\'re you doing to the missle?'}
         if ws.recv_data_frame()[1].data.strip() != success_msg:
+            reverse_proxy.kill()
             return {'status': 'down', 'msg': 'You destroyed a wrong target'}
         logs = requests.get("http://" + host + ":" + str(port) + "/log").text
         if success_msg in logs:
+            reverse_proxy.kill()
             return {'status': 'up', 'msg': 'ok'}
         else:
+            reverse_proxy.kill()
             return {'status': 'down', 'msg': 'log feature is not functioning.'}
+    reverse_proxy.kill()
+    return {'status': 'error', 'msg': 'unexpetected checking path.'}
 
 
 
