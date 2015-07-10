ad5bf533408be52d223d56c0cd726f1ea903c62b
Qijiang Fan <fqj1994@gmail.com>
2015-07-10 22:47:14 +0800

    ..

--- checker/missle.py
+++ checker/missle.py
@@ -5,8 +5,12 @@ import json
 
 def checker(host, port, flag, team):
     ooo = json.dumps([host, port, flag, team]).encode('base64').replace('\n', '')
-    xx = os.popen("echo " + ooo + " | base64 -d | /home/xctf/xctf-final-2015/scripts/service/missle/missle_checker.py " + team) 
+    xx = os.popen("echo " + ooo + " | base64 -d | (timeout 20 /home/xctf/xctf-final-2015/scripts/service/missle/missle_checker.py " + team + "|| echo checker timeout)") 
     read_data = xx.read()
-    ret = json.loads(read_data)
+    ret = None
+    try:
+        ret = json.loads(read_data)
+    except:
+        ret = {'status': 'error', 'msg': read_data.strip()}
     xx.close()
     return ret
