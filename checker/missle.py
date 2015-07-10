c8a6471f8b28a557acb6fe8953438b3a9681e247
Qijiang Fan <fqj1994@gmail.com>
2015-07-10 22:42:36 +0800

    ..

--- checker/missle.py
+++ checker/missle.py
@@ -2,12 +2,10 @@
 
 import os
 import json
-import pty
 
 def checker(host, port, flag, team):
-    master, slave = pty.openpty()
     ooo = json.dumps([host, port, flag, team]).encode('base64').replace('\n', '')
-    xx = os.popen("echo " + ooo + " | base64 -d | /home/xctf/xctf-final-2015/scripts/service/missle/missle_checker.py") 
+    xx = os.popen("echo " + ooo + " | base64 -d | /home/xctf/xctf-final-2015/scripts/service/missle/missle_checker.py " + team) 
     read_data = xx.read()
     ret = json.loads(read_data)
     xx.close()
